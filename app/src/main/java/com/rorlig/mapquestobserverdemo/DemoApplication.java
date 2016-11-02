dca9ea07442a29e572b175945340229cbc9b69cb
taranro <roy.tarantino@gmail.com>
2016-11-02 10:23:59 -0400

    Added mdn for tracking id and device id Added sequence numbers to additional data in observer changed builder to get a location every 5 minutes

--- app/src/main/java/com/rorlig/mapquestobserverdemo/DemoApplication.java
+++ app/src/main/java/com/rorlig/mapquestobserverdemo/DemoApplication.java
@@ -1,6 +1,9 @@
 package com.rorlig.mapquestobserverdemo;
 
 import android.app.Application;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.mapquest.tracking.AdditionalObservationProvider;
 import com.mapquest.tracking.LocationTracker;
@@ -14,6 +17,10 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class DemoApplication extends Application {
+    private final String TAG= "DemoApplication";
+    private final String APIKEY= "jzHKWTrSFAPmu5eLfEv09pF0tIiAZ3pn";
+    private final String DEVID= "33416657-377e-4621-b919-8d141befadb9";
+    static int seq=-1;
 
     @Override
     public void onCreate() {
@@ -23,18 +30,37 @@ public class DemoApplication extends Application {
 
 
     private void initializeTracker() {
-        new LocationTracker.Builder(this, "LjeS0kYCqo33EqqwN9iW7A9s7jBj0meT") // TODO Update with your access key
-                .withActiveLocationUpdatesDuringPassiveListening(TimeUnit.SECONDS.toMillis(15), true)
-                .withPassiveListeningMinimumLocationUpdateIntervalDuration(TimeUnit.MINUTES.toMillis(3))
-                .withPassiveListeningMinimumLocationUpdateDistance(0)
-                .withProperty("deliveryNumber", 1L)
-                .withProperty("driverEmployeeId", "ABC123")
+
+        Context mContext = getApplicationContext();
+
+        TelephonyManager tMgr = (TelephonyManager)mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        final String mPhoneNumber = tMgr.getLine1Number();
+        final String mObserved = "location observed: "+ mPhoneNumber;
+
+
+
+
+        new LocationTracker.Builder(this, APIKEY)
+                .withActiveLocationUpdatesDuringPassiveListening(TimeUnit.SECONDS.toMillis(300), true)      // location every 5 minutes if no passive loc avail, wake up for it.
+                .withPassiveListeningMinimumLocationUpdateIntervalDuration(TimeUnit.MINUTES.toMillis(2))    // wait at most 2 minutes between passive locations
+                .withPassiveListeningMinimumLocationUpdateDistance(0)                                       // 0 meters between locations
+                .withWakeForReporting(true)                                                                 // Wake up for reporting
+                .withReportingIntervalDuration(TimeUnit.MINUTES.toMillis(1)   )                             // report every minute (default)
+
+                .withDeviceId(mPhoneNumber + "-PassiveActive")                                              // put mdn in device id
+                .withTrackingId(mPhoneNumber)
                 .withAdditionalObservationProvider(new AdditionalObservationProvider() {
                     @Override
                     public Map<String, Double> getAdditionalObservationData() {
-                        return Collections.singletonMap("temperature", 23.4);
+                        seq++;
+                        Log.d(TAG,mObserved + " seq: " + Integer.toString(seq));
+
+                        return Collections.singletonMap("sequence", (double) seq);
                     }
-                }).buildAndInitialize();
+                }
+                )
+
+                .buildAndInitialize();
     }
 
 }
