a8891d07955f2b9d9b8a96ee6de07e2ccf436192
taranro <roy.tarantino@gmail.com>
2016-12-20 16:04:34 -0500

    Updated locationbuilder to test observed data.

--- app/src/main/java/com/rorlig/mapquestobserverdemo/DemoApplication.java
+++ app/src/main/java/com/rorlig/mapquestobserverdemo/DemoApplication.java
@@ -9,8 +9,10 @@ import com.mapquest.tracking.AdditionalObservationProvider;
 import com.mapquest.tracking.LocationTracker;
 
 import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.lang.*;
+
 
 /**
  * @author gaurav gupta
@@ -21,6 +23,7 @@ public class DemoApplication extends Application {
     private final String APIKEY= "jzHKWTrSFAPmu5eLfEv09pF0tIiAZ3pn";
     private final String DEVID= "33416657-377e-4621-b919-8d141befadb9";
     static int seq=-1;
+    static double ds = -1.0;
 
     @Override
     public void onCreate() {
@@ -40,6 +43,8 @@ public class DemoApplication extends Application {
 
 
 
+
+
         new LocationTracker.Builder(this, APIKEY)
 //                .withActiveLocationUpdatesDuringPassiveListening(TimeUnit.SECONDS.toMillis(300), true)      // location every 5 minutes if no passive loc avail, wake up for it.
 //                .withPassiveListeningMinimumLocationUpdateIntervalDuration(TimeUnit.MINUTES.toMillis(2))    // wait at most 2 minutes between passive locations
@@ -50,13 +55,24 @@ public class DemoApplication extends Application {
 //                .withDeviceId(mPhoneNumber + "-PassiveActive")                                              // put mdn in device id
                 .withDeviceId(mPhoneNumber + "-Active")                                              // put mdn in device id
                 .withTrackingId(mPhoneNumber)
+                .withProperty("p1", 123456789)
+                .withProperty("p2", "property string p2")
                 .withAdditionalObservationProvider(new AdditionalObservationProvider() {
                     @Override
                     public Map<String, Double> getAdditionalObservationData() {
-                        seq++;
-                        Log.d(TAG,mObserved + " seq: " + Integer.toString(seq));
 
-                        return Collections.singletonMap("sequence", (double) seq);
+                        Map<String, Double>  myMap = new HashMap<String, Double>();
+
+                        seq++;
+                        ds++;
+
+                        myMap.put("ObservationData1_ds", ds);
+                        myMap.put("ObservationData2_ds_times_2", 2 * ds);
+                        myMap.put("ObservationData3_ds_squared", ds * ds);
+
+                        Log.d(TAG,mObserved + " seq: " + Integer.toString(seq) + " " + String.valueOf(ds));
+//                        return Collections.singletonMap("sequence", ds);
+                        return myMap;
                     }
                 }
                 )
